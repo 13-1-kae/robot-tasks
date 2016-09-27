c84a5f75ac80aebbe45b27293ee9c781baf68b88
chelberserker <bersenjow@gmail.com>
2016-09-27 11:38:15 +0300

    19pocleproverki

--- task_19.py
+++ task_19.py
@@ -5,6 +5,22 @@ from pyrob.api import *
 
 @task
 def task_8_29():
+    while wall_is_above() and not wall_is_on_the_left():
+        move_left()
+    if wall_is_on_the_left():
+        while wall_is_above() and not wall_is_on_the_right():
+            move_right()
+        if not wall_is_above():
+            while not wall_is_above():
+                move_up()
+            while not wall_is_on_the_left():
+                move_left()
+    else:
+        while not wall_is_above():
+                move_up()
+        while not wall_is_on_the_left():
+                move_left()
+
     pass
 
 
