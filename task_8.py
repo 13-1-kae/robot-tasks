be5f8bb143d50395002d8e6b1e67440146bc0942
chelberserker <bersenjow@gmail.com>
2016-09-27 10:30:36 +0300

    t 1.10

--- task_8.py
+++ task_8.py
@@ -5,8 +5,12 @@ from pyrob.api import *
 
 @task
 def task_5_7():
+    while wall_is_beneath() or wall_is_above():
+        move_right()
     pass
+
 
 
 if __name__ == '__main__':
     run_tasks()
+
