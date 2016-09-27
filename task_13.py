3bcae2af2f88e8ca04e9df8bf576f8e6b076111a
chelberserker <bersenjow@gmail.com>
2016-09-27 10:40:05 +0300

    t 1.13

--- task_13.py
+++ task_13.py
@@ -5,6 +5,36 @@ from pyrob.api import *
 
 @task
 def task_8_10():
+    while not wall_is_on_the_right():
+        if not wall_is_above() and not wall_is_beneath():
+            move_up()
+            fill_cell()
+            move_down(2)
+            fill_cell()
+            move_up()
+        elif not wall_is_above():
+            move_up()
+            fill_cell()
+            move_down()
+        elif not wall_is_beneath():
+            move_down()
+            fill_cell()
+            move_up()
+        move_right()
+    if not wall_is_above() and not wall_is_beneath():
+            move_up()
+            fill_cell()
+            move_down(2)
+            fill_cell()
+            move_up()
+    elif not wall_is_above():
+            move_up()
+            fill_cell()
+            move_down()
+    elif not wall_is_beneath():
+            move_down()
+            fill_cell()
+            move_up()
     pass
 
 
