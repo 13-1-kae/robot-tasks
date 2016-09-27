de49401aeffff1adc6551d342f028c9a1da7e34c
chelberserker <bersenjow@gmail.com>
2016-09-27 11:10:53 +0300

    t 1.18

--- task_18.py
+++ task_18.py
@@ -5,7 +5,21 @@ from pyrob.api import *
 
 @task
 def task_8_28():
+    while wall_is_above() and not wall_is_on_the_right():
+        move_right()
+    if not wall_is_above():
+        move_up()
+    else:
+        while wall_is_above() and not wall_is_on_the_left():
+            move_left()
+        move_up()
+    while not wall_is_on_the_left():
+        move_left()
+    while not wall_is_above():
+        move_up()
+
     pass
+
 
 
 if __name__ == '__main__':
