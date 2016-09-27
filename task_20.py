8e0633259f9056df6cedbe9e77bd2651b4fb9757
chelberserker <bersenjow@gmail.com>
2016-09-27 12:03:59 +0300

    20pocleproverki

--- task_20.py
+++ task_20.py
@@ -5,7 +5,32 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
+    move_right()
+    fill_cell()
+    for i in range(6):
+        fill_cell()
+        for j in range (26):
+            move_right()
+            fill_cell()
+        move_down()
+        fill_cell()
+        for j in range (26):
+            move_left()
+            fill_cell()
+        move_down()
+
+
+
+
+
+
+
+
+
+
+
     pass
+
 
 
 if __name__ == '__main__':
